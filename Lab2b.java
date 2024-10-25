622d66479a7723aaeb32da35ee40ba4c38757aac
Yash Shekhar <yashshekhar2934@gmail.com>
2024-10-25 19:55:28 +0530

    Lab 2 second solution

---
+++ Lab2b.java
@@ -0,0 +1,16 @@
+public class LeapYearCheck {
+    public static void main(String[] args) {
+        int year = 2024; // You can change the year value for testing
+
+        // Check if the year is a leap year
+        if (year % 400 == 0) {
+            System.out.println(year + " is a leap year.");
+        } else if (year % 100 == 0) {
+            System.out.println(year + " is not a leap year.");
+        } else if (year % 4 == 0) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+    }
+}
